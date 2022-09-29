b19ccd069e67a6d4e963518f0e03053e8516ebc8
NadaElho <nadaelhosary51@gmail.com>
2022-09-29 22:44:51 +0200

    project update commited again

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let title= document.querySelector(".title");
 let advice= document.querySelector(".advice")
-fetch("https://api.adviceslip.com/advice").then((res)=>{
+fetch("https://api.adviceslip.com/advice",{cache: "no-cache"}).then((res)=>{
     return res.json();
 }).then(data=>{
     title.innerHTML=`Advice #${data.slip.id}`;
-    advice.innerHTML=`"${data.slip.advice}"`
+    advice.innerHTML=`<blockquote>${data.slip.advice}</blockquote>`
 })
